932e94db21bb54e146595b641449ac45971f6cd8
Keila Pitangui <kmpitangui@gmail.com>
2026-01-29 16:00:51 -0300

    Mudando regras de droplet

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "digitalocean_project" "rancher_rke" {
-  name        = "Rancher_RKE2"
-  description = "A project to represent development resources."
-  purpose     = "Web Application"
-  environment = "Development"
-}
-
 module "meus_servidores" {
   source = "./modules/vms-rancher"
 }
